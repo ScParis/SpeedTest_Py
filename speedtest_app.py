f83c21445075f5053f8c91e23b958a7dc2f741b5
Scherman Paris <schpariss@gmail.com>
2023-05-30 23:01:22 -0300

    Upgrading speedtest_app

--- speedtest_app.py
+++ speedtest_app.py
@@ -4,37 +4,58 @@ import datetime
 import platform
 import netifaces
 import re
+import shutil
+from tqdm import tqdm
+
+
+def move_logs_to_saved(log_folder, log_saved_folder):
+    if not os.path.exists(log_saved_folder):
+        os.makedirs(log_saved_folder)
+
+    logs = os.listdir(log_folder)
+    for log_file in logs:
+        if log_file.endswith(".txt"):
+            source = os.path.join(log_folder, log_file)
+            destination = os.path.join(log_saved_folder, log_file)
+            shutil.move(source, destination)
 
 
 def run_speedtest():
-    # Comando para executar o Speedtest CLI com a opção --accept-license
     command = "./speedtest"
     log_folder = "Logs"
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    log_file = os.path.join(log_folder, f"speedtest_log_{timestamp}.txt")
+    log_saved_folder = os.path.join(log_folder, "log_saved")
+
+    # Move os logs antigos para a pasta "log_saved"
+    move_logs_to_saved(log_folder, log_saved_folder)
 
     try:
         start_time = datetime.datetime.now()
 
-        # Executa o comando do Speedtest CLI e captura a saída
-        output = subprocess.check_output(command.split())
-        output = output.decode("utf-8")  # Decodifica a saída para uma string
+        output = ""
+
+        process = subprocess.Popen(
+            command.split(), stdout=subprocess.PIPE, universal_newlines=True)
+
+        progress_bar = tqdm(total=100, desc="Executando Speedtest")
+
+        for line in process.stdout:
+            output += line
+            progress_bar.update(10)
+
+        process.wait()
+        output = output.strip()
 
         end_time = datetime.datetime.now()
         total_time = end_time - start_time
 
-        # Verifica se a pasta de log existe e a cria, se necessário
         if not os.path.exists(log_folder):
             os.makedirs(log_folder)
 
-        # Obtém informações da conexão
         connection_type = "WiFi" if is_wifi_connected() else "Cabo"
         wifi_frequency = get_wifi_frequency()
 
-        # Obtém informações do sistema operacional
         os_info = platform.system() + " " + platform.release()
 
-        # Constrói o registro com as informações coletadas
         log_entry = f"Data/Hora: {end_time}\n"
         log_entry += f"Tempo Total: {total_time}\n"
         log_entry += f"Conexão: {connection_type}\n"
@@ -42,13 +63,14 @@ def run_speedtest():
         log_entry += f"Sistema Operacional: {os_info}\n"
         log_entry += f"Resultado:\n{output}\n"
 
-        # Salva o registro no arquivo de log
+        log_file = os.path.join(
+            log_folder, f"speedtest_log_{end_time.strftime('%Y%m%d_%H%M%S')}.txt")
+
         with open(log_file, "a") as file:
             file.write(log_entry)
-            
-        print("Conteúdo de 'output':")
         print(output)
         print("Speedtest concluído com sucesso. Registro salvo em", log_file)
+
     except subprocess.CalledProcessError as e:
         print("Ocorreu um erro ao executar o Speedtest CLI:", e)
 
@@ -64,13 +86,12 @@ def is_wifi_connected():
 
 
 def get_wifi_frequency():
-    command = "iwconfig wlp1s0"  # Substitua 'wlan0' pelo nome da sua interface WiFi
+    command = "iwconfig wlp1s0"
 
     try:
         output = subprocess.check_output(
             command.split(), universal_newlines=True)
 
-        # Use expressões regulares para encontrar a frequência da rede WiFi
         frequency_match = re.search(r'Frequency:([\d.]+) GHz', output)
         if frequency_match:
             frequency = float(frequency_match.group(1))
